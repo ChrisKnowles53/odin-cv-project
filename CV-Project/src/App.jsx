e2e61a5f1ee928e492489c3568626caf3463c510
Chris Knowles <chris.knowles@thinkmoney.co.uk>
2023-12-11 08:43:43 +0000

    project boilerplate setup

---
+++ CV-Project/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main-contianer">
+      <h1>CJK's CV Webpage</h1>
+      <h2>Created using Vite, React and Javascript</h2>
+    </div>
+  );
+}
+
+export default App;
