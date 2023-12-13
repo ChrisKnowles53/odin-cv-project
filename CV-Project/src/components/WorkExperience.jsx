2ab0887a522aee575c77dc3789828d6796139f7e
Chris Knowles <chris.knowles@thinkmoney.co.uk>
2023-12-13 15:42:06 +0000

    minor updates to tidy the code

--- CV-Project/src/components/WorkExperience.jsx
+++ CV-Project/src/components/WorkExperience.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import InputField from "./Input";
-import Button from "./Button";
 import DateInputField from "../DateInput";
 import React from "react";
 
@@ -11,16 +10,12 @@ export default function WorkExperience({ section }) {
     position: "Enter position",
     title: "Enter title",
     mainResponsibilities: "Enter main responsibilities",
-    // dateFrom: "Enter start date",
-    // dateTo: "Enter end date",
   });
   const inputFields = [
     { key: "companyName", label: "Company Name" },
     { key: "position", label: "Position" },
     { key: "title", label: "Title" },
     { key: "mainResponsibilities", label: "Main Responsibilities" },
-    // { key: "dateFrom", label: "Date From" },
-    // { key: "dateTo", label: "Date To" },
   ];
 
   const handleInputChange = (key, value) => {
@@ -29,10 +24,6 @@ export default function WorkExperience({ section }) {
       [key]: value,
     }));
   };
-
-  function handleClick() {
-    // do something with the click
-  }
 
   return (
     <div className={section}>
@@ -53,8 +44,6 @@ export default function WorkExperience({ section }) {
       <DateInputField label="Date from" />
       <br />
       <DateInputField label="Date to" />
-      <Button text="Save" handleClick={handleClick} />
-      <Button text="Edit" />
     </div>
   );
 }
